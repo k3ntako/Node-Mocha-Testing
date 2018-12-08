fa97322ba1f3a2d0a7a7cc9d4428a75721e51fcc
Kentaro Kaneki <k@Ken-2.local>
2018-12-08 12:30:09 -0500

    Describe blocks

--- server/server.test.js
+++ server/server.test.js
@@ -3,54 +3,64 @@ const expect = require('expect');
 
 let app = require('./server').app;
 
-it('should return hello test response', (done) => {
-  request(app)
-    .get('/')
-    .expect(200)
-    .expect('Hello test.')
-    .end(done);
-});
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return hello test response', (done) => {
+      request(app)
+        .get('/')
+        .expect(200)
+        .expect('Hello test.')
+        .end(done);
+    });
+  });
 
-it('should return all todos', (done) => {
-  request(app)
-    .get('/api/v1/todos')
-    .expect(200)
-    .expect({
-      todos: [
-        {
-          todo: "Homework",
-          completed: false
-        },
-        {
-          todo: "Walk the dog",
-          completed: false
-        }
-      ]
-    })
-    .end(done);
-});
+  describe('GET /api/v1/todos', () => {
+    it('should return all todos', (done) => {
+      request(app)
+        .get('/api/v1/todos')
+        .expect(200)
+        .expect({
+          todos: [
+            {
+              todo: "Homework",
+              completed: false
+            },
+            {
+              todo: "Walk the dog",
+              completed: false
+            }
+          ]
+        })
+        .end(done);
+    });
+  });
 
-it('should include Kentaro', (done) => {
-  request(app)
-    .get('/api/v1/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.users).toInclude({
-        name: "Kentaro",
-        age: 23
-      })
-    })
-    .end(done);
-});
+  describe('GET /api/v1/users', () => {
+    it('should include Kentaro', (done) => {
+      request(app)
+        .get('/api/v1/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.users).toInclude({
+            name: "Kentaro",
+            age: 23
+          })
+        })
+        .end(done);
+    });
+  });
 
-it('should return hello test response', (done) => {
-  request(app)
-    .get('/test')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
+  describe('GET /test', () => {
+    it('should return hello test response', (done) => {
+      request(app)
+        .get('/test')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,41 +1,50 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  let res = utils.add(33,11);
-  expect(res).toBe(44).toBeA('number');
-});
 
-it('should square number', () => {
-  let res = utils.square(9);
-  expect(res).toBe(81).toBeA('number');
-});
+describe('Utils', () => {
+  describe('add', () => {
+    it('should add two numbers', () => {
+      let res = utils.add(33,11);
+      expect(res).toBe(44).toBeA('number');
+    });
 
-it('should set first name and last names', () => {
-  let user = {
-    location: "Brookline, MA",
-    age: 23
-  };
-
-  let res = utils.setName(user, "Kentaro Kaneki");
-  expect(res).toInclude({
-    firstName: "Kentaro",
-    lastName: "Kaneki"
+    it('should sum two numbers', (done) => {
+      utils.asyncAdd(4,3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
   });
 
-  expect(res).toBeA('object');
-});
+  describe('square', () => {
+    it('should square number', () => {
+      let res = utils.square(9);
+      expect(res).toBe(81).toBeA('number');
+    });
 
-it('should sum two numbers', (done) => {
-  utils.asyncAdd(4,3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+    it('should square a number', (done) => {
+      utils.asyncSquare(4, (square) => {
+        expect(square).toBe(16).toBeA('number');
+        done();
+      });
+    });
   });
-});
 
-it('should square a number', (done) => {
-  utils.asyncSquare(4, (square) => {
-    expect(square).toBe(16).toBeA('number');
-    done();
+  describe('#user', () => {
+    it('should set first name and last names', () => {
+      let user = {
+        location: "Brookline, MA",
+        age: 23
+      };
+
+      let res = utils.setName(user, "Kentaro Kaneki");
+      expect(res).toInclude({
+        firstName: "Kentaro",
+        lastName: "Kaneki"
+      });
+
+      expect(res).toBeA('object');
+    });
   });
 });
